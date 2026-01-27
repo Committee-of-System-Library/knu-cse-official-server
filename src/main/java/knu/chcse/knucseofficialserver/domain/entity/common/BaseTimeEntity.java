55acb8bd88b2e10bd67bb1ed8fc59a9f2b1e4e76
yu jaehwan <dkflawoghks123456@gmail.com>
2026-01-27 22:25:40 +0900

    feat : BaseTime entity 생성

---
+++ src/main/java/knu/chcse/knucseofficialserver/domain/entity/common/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package knu.chcse.knucseofficialserver.domain.entity.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Column(name = "created_at", updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
