04cbad402cacb938bb3f811d2f3d0e364c540129
yu jaehwan <dkflawoghks123456@gmail.com>
2026-01-28 00:50:24 +0900

    fix : ID Long type 변경

--- src/main/java/knu/chcse/knucseofficialserver/domain/entity/comment/CommentJpaRepository.java
+++ src/main/java/knu/chcse/knucseofficialserver/domain/entity/comment/CommentJpaRepository.java
@@ -2,5 +2,5 @@ package knu.chcse.knucseofficialserver.domain.entity.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentJpaRepository extends JpaRepository<Comment, Integer> {
+public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 }
