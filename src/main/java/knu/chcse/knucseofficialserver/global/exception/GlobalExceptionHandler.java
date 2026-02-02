6b8b4cf8b17cc49dbfc43fb04826fc2d3931a212
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-02 22:38:55 +0900

    fix : 사용하지 않는 import 제거 및 return type 변경

--- src/main/java/knu/chcse/knucseofficialserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/exception/GlobalExceptionHandler.java
@@ -2,18 +2,22 @@ package knu.chcse.knucseofficialserver.global.exception;
 
 import knu.chcse.knucseofficialserver.global.error.ErrorResponse;
 import knu.chcse.knucseofficialserver.global.error.ErrorResponseImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ErrorResponse handleCustomRuntimeException(BusinessException ex){
-        return ErrorResponseImpl.builder()
+    public ResponseEntity<ErrorResponse> handleCustomRuntimeException(BusinessException ex){
+         ErrorResponse response = ErrorResponseImpl.builder()
                 .code(ex.getCode())
                 .message(ex.getMessage())
                 .build();
+
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
