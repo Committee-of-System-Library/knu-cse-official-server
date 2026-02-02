c1cc00c23c4b7cbc22abcc35bda12a2bac6921f8
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-03 01:34:04 +0900

    fix : 생성자에 httpstatus field 추가

--- src/main/java/knu/chcse/knucseofficialserver/global/error/CommonErrorCode.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/error/CommonErrorCode.java
@@ -1,17 +1,26 @@
 package knu.chcse.knucseofficialserver.global.error;
 
-public enum CommonErrorCode implements ErrorCode {
-    INVALID_REQUEST("C001", "잘못된 요청입니다."),
-    NOT_FOUND("C002", "데이터를 찾을 수 없습니다."),
-    UNAUTHORIZED("C003", "권한이 없습니다.");
+import org.springframework.http.HttpStatus;
 
+public enum CommonErrorCode implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND,"C002", "데이터를 찾을 수 없습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"C003", "권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 
-    CommonErrorCode(String code, String message) {
+    CommonErrorCode(HttpStatus httpStatus,String code, String message) {
+        this.httpStatus = httpStatus;
         this.code = code;
         this.message = message;
     }
+
+    @Override
+    public HttpStatus HttpStatus() {
+        return httpStatus;
+    }
+
 
     @Override
     public String code(){
