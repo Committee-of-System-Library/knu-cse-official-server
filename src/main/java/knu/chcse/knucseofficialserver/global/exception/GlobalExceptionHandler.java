2128f1ab27e2c87f996c6298926f0fdac747efa5
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-03 01:33:09 +0900

    fix : handler status와 body 수정

--- src/main/java/knu/chcse/knucseofficialserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package knu.chcse.knucseofficialserver.global.exception;
 
+import knu.chcse.knucseofficialserver.global.error.ErrorCode;
 import knu.chcse.knucseofficialserver.global.error.ErrorResponse;
 import knu.chcse.knucseofficialserver.global.error.ErrorResponseImpl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,11 +13,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleCustomRuntimeException(BusinessException ex){
-         ErrorResponse response = ErrorResponseImpl.builder()
-                .code(ex.getCode())
-                .message(ex.getMessage())
-                .build();
 
-         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        ErrorCode errorCode = ex.getErrorCode();
+
+         return ResponseEntity
+                 .status(errorCode.HttpStatus())
+                 .body(ErrorResponseImpl.builder()
+                         .code(errorCode.code())
+                         .message(errorCode.message())
+                         .build());
     }
 }
