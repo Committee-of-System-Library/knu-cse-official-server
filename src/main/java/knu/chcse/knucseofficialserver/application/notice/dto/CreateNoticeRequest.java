553e88ae5ed2ee30214aa8a0950dfa13c92fba77
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-03 01:13:28 +0900

    feat : Jackson으로 인해 기본생성자, setter dto에 추가

--- src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
@@ -1,8 +1,12 @@
 package knu.chcse.knucseofficialserver.application.notice.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class CreateNoticeRequest {
     private String title;
     private String content;
