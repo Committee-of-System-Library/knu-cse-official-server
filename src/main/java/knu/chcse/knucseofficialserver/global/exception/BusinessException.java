99f0a04336462888cd632203fddbc174e6efcad4
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-03 01:33:29 +0900

    fix : Objects class로 npe 처리

--- src/main/java/knu/chcse/knucseofficialserver/global/exception/BusinessException.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/exception/BusinessException.java
@@ -3,12 +3,15 @@ package knu.chcse.knucseofficialserver.global.exception;
 import knu.chcse.knucseofficialserver.global.error.ErrorCode;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
-        super(errorCode.message());
+        //super로 인한 npe 방지. super는 항상 첫줄에 써야하므로, Objects로 처리
+        super(Objects.requireNonNull(errorCode, "errorCode must not be null").message());
         this.errorCode = errorCode;
     }
 
