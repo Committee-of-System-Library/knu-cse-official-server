98ec36b68e6fb3aa4ac53445ff5eaa7c0aa17b5e
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-02 22:43:50 +0900

    feat : NoticeService interface 추가.(crud)

---
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/NoticeService.java
@@ -0,0 +1,20 @@
+package knu.chcse.knucseofficialserver.application.notice;
+
+import jakarta.transaction.Transactional;
+import knu.chcse.knucseofficialserver.application.notice.dto.CreateNoticeRequest;
+import knu.chcse.knucseofficialserver.application.notice.dto.NoticeResponse;
+import knu.chcse.knucseofficialserver.application.notice.dto.UpdateNoticeRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//interface 설계
+public interface NoticeService {
+    Long createNotice(CreateNoticeRequest request);
+    NoticeResponse getNotice(Long noticeId);
+    List<NoticeResponse> getNotices();
+    void updateNotice(Long noticeId, UpdateNoticeRequest request);
+    void deleteNotice(Long noticeId);
+
+}
