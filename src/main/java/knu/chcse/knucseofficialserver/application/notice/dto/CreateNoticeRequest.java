bc5d60c05a0e5806e320ab8bad4aecc76f404490
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-04 00:14:35 +0900

    fix : record로 변경

--- src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
@@ -4,10 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class CreateNoticeRequest {
-    private String title;
-    private String content;
-}
+
+public record CreateNoticeRequest (
+    String title,
+    String content
+){}
--- src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/UpdateNoticeRequest.java
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/UpdateNoticeRequest.java
@@ -4,10 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class UpdateNoticeRequest {
-    private String title;
-    private String content;
-}
+
+public record UpdateNoticeRequest (
+    String title,
+    String content
+){}
