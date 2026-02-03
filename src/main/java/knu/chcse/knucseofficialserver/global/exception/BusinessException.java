beb45677a8805e32a0ade9b93d152ff97295c17f
yu jaehwan <dkflawoghks123456@gmail.com>
2026-02-04 00:14:47 +0900

    fix : 주석제거

--- src/main/java/knu/chcse/knucseofficialserver/global/exception/BusinessException.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/exception/BusinessException.java
@@ -10,7 +10,6 @@ public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
-        //super로 인한 npe 방지. super는 항상 첫줄에 써야하므로, Objects로 처리
         super(Objects.requireNonNull(errorCode, "errorCode must not be null").message());
         this.errorCode = errorCode;
     }
