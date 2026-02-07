5bcf8f8a139b942ea0e6e936f0dd0bf6737df930
김동우 <dongwoo143@gmail.com>
2026-02-08 06:50:12 +0900

    feat: 공지사항 API에 유효성 검증 추가

--- src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/CreateNoticeRequest.java
@@ -1,6 +1,13 @@
 package knu.chcse.knucseofficialserver.application.notice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateNoticeRequest (
+    @NotBlank(message = "제목은 필수 입력 항목입니다.")
+    @Size(max = 100, message = "제목은 100자를 초과할 수 없습니다.")
     String title,
+
+    @NotBlank(message = "내용은 필수 입력 항목입니다.")
     String content
 ){}
