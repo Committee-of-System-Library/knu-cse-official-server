9325d3fd594153870b098c9bf8b5be00d81c282d
김동우 <dongwoo143@gmail.com>
2026-02-07 19:59:41 +0900

    refactor: 사용하지 않는 lombok import 정리

--- src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/UpdateNoticeRequest.java
+++ src/main/java/knu/chcse/knucseofficialserver/application/notice/dto/UpdateNoticeRequest.java
@@ -1,10 +1,5 @@
 package knu.chcse.knucseofficialserver.application.notice.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
 public record UpdateNoticeRequest (
     String title,
     String content
