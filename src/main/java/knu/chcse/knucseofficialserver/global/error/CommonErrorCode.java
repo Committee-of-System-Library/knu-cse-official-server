944dc78e7e7b49e6a4b21d38248f58798ff478e9
김동우 <dongwoo143@gmail.com>
2026-02-08 06:53:35 +0900

    feat: 학번 불일치 시 404 대신 401 반환하도록 INVALID_CREDENTIALS 에러 코드 추가

--- src/main/java/knu/chcse/knucseofficialserver/global/error/CommonErrorCode.java
+++ src/main/java/knu/chcse/knucseofficialserver/global/error/CommonErrorCode.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum CommonErrorCode implements ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND,"C002", "데이터를 찾을 수 없습니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"C003", "권한이 없습니다.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"C003", "권한이 없습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED,"C004", "인증 정보가 올바르지 않습니다.(일치하는 학번이 없음)");
 
     private final HttpStatus httpStatus;
     private final String code;
